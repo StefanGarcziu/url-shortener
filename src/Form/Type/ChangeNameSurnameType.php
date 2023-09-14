7430b4270179f0e4491f43c65ba8a5b6bb615ae4
Stepan Garciu <74588873+StefanGarcziu@users.noreply.github.com>
2023-09-14 17:21:36 +0200

    Add files via upload

---
+++ src/Form/Type/ChangeNameSurnameType.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Change name surname type.
+ */
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Change name surname type class.
+ */
+class ChangeNameSurnameType extends AbstractType
+{
+    /**
+     * Build form.
+     *
+     * @param FormBuilderInterface $builder Builder
+     * @param array                $options Options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'required' => true,
+                'attr' => ['min_length' => 3, 'max_length' => 45],
+            ]);
+        $builder
+            ->add('surname', TextType::class, [
+                'required' => true,
+                'attr' => ['min_length' => 3, 'max_length' => 45],
+            ]);
+    }
+
+    /**
+     * Returns the prefix of the template block name for this type.
+     *
+     * The block prefix defaults to the underscored short class name with
+     * the "Type" suffix removed (e.g. "UserProfileType" => "user_profile").
+     *
+     * @return string The prefix of the template block name
+     */
+    public function getBlockPrefix(): string
+    {
+        return 'user';
+    }
+}
